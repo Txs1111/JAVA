afb1247d8a26db83ec98636f742008a30d2726a4
Txs1111 <1831069264@qq.com>
2021-05-11 17:16:18 +0800

    练习File

--- IO/src/comT_Lyon/newFile/TestFile.java
+++ IO/src/comT_Lyon/newFile/TestFile.java
@@ -1,5 +1,15 @@
 package comT_Lyon.newFile;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 public class TestFile {
 
+    @Test
+    public void createFile(){
+        File a= new File("实验路径\\a.txt");
+        //并不会创建文件
+    }
 }
