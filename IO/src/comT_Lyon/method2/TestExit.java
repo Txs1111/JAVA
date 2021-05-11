9a9cdbd35031032f5602a7fad646c61344ea69b1
Txs1111 <1831069264@qq.com>
2021-05-11 17:31:44 +0800

    练习exists isFile isDirectory

---
+++ IO/src/comT_Lyon/method2/TestExit.java
@@ -0,0 +1,22 @@
+package comT_Lyon.method2;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestExit {
+    @Test
+    public void createFile() {
+
+        File a = new File("D:\\IDEA\\Code\\JAVA\\IO\\src\\comT_Lyon\\method2\\TestExit.java");
+        File a2 = new File("D:\\IDEA\\Code\\JAVA\\IO\\src\\comT_Lyon\\method2\\实验路径");
+        System.out.println(a.getPath());
+        System.out.println("实验路径 是否存在:"+a2.exists());
+        System.out.println("实验路径\\a.txt 是否存在:"+a.exists());
+        // 判断是文件还是目录
+        System.out.println("实验路径 文件?:"+a2.isFile());
+        System.out.println("实验路径 目录?:"+a2.isDirectory());
+
+
+    }
+}
