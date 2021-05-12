5ce3ae0cca76a5c864fb26a3f6725086a2cdd7e6
Txs1111 <1831069264@qq.com>
2021-05-12 18:40:28 +0800

    遍历所有文件

---
+++ IO/src/comT_Lyon/method4/TestEach.java
@@ -0,0 +1,39 @@
+package comT_Lyon.method4;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestEach {
+    @Test
+    public void createFile() throws Exception {
+        File file = new File("c:");
+        String[] list = file.list();
+        for (String lists : list) {
+            System.out.println(lists);
+        }
+        File[] file2 = file.listFiles();
+        for (File file2s : file2
+        ) {
+            System.out.println(file2s);
+        }
+    }
+
+    @Test
+    public void dgAllData() {
+        File file = new File("F:\\校园文件");
+        dg(file);
+    }
+
+    public void dg(File file) {
+        if (!file.isDirectory()) {
+            System.out.println(file.getPath());
+            return;
+        } else {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                dg(f);
+            }
+        }
+    }
+}
