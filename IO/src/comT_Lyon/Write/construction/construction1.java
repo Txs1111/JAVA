bbf62bc954a223c44a0b3ba00298b4615438f326
Txs1111 <1831069264@qq.com>
2021-05-13 11:37:24 +0800

    Write的构造方法

---
+++ IO/src/comT_Lyon/Write/construction/construction1.java
@@ -0,0 +1,19 @@
+package comT_Lyon.Write.construction;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class construction1 {
+    @Test
+    public void demo1() throws Exception {
+
+        File file = new File("src\\comT_Lyon\\Write\\construction\\a.txt");
+        FileWriter fileWriter = new FileWriter(file);
+
+
+        FileWriter fileWriter1 = new FileWriter("src\\comT_Lyon\\Write\\construction\\a.txt");
+    }
+}
