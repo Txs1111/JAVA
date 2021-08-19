dcca7f9d59e2b2742dc8d40a456ce0003ba43f39
L_yon <1831069264@qq.com>
2021-08-19 15:37:32 +0800

    update

---
+++ ServletTest/src/main/java/com/tlyon/Servlet监听器统计在线人数/CharactorFilter.java
@@ -0,0 +1,36 @@
+package com.tlyon.Servlet监听器统计在线人数;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+// 字符编码过滤器
+public class CharactorFilter implements Filter {
+	// 字符编码
+	String encoding = null;
+	public void destroy() {
+		encoding = null;
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+						 FilterChain chain) throws IOException, ServletException {
+		// 判断字符编码是否为空
+		if(encoding != null){
+			// 设置request的编码格式
+			request.setCharacterEncoding(encoding);
+			// 设置response字符编码
+			response.setContentType("text/html; charset="+encoding);
+		}
+		// 传递给下一过滤器
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// 获取初始化参数
+		encoding = filterConfig.getInitParameter("encoding");
+	}
+
+}
---
+++ ServletTest/src/main/java/com/tlyon/Servlet监听器统计在线人数/MyContentListener.java
@@ -0,0 +1,35 @@
+package com.tlyon.Servlet监听器统计在线人数;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * Application Lifecycle Listener implementation class MyContentListener
+ *
+ */
+@WebListener("This is only a demo listener") 
+public class MyContentListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MyContentListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
