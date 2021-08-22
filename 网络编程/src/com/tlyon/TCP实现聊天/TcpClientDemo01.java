12875afdbae57f7bdb5788c375aa8977104de8b6
L_yon <1831069264@qq.com>
2021-08-22 15:29:50 +0800

    update

---
+++ 网络编程/src/com/tlyon/TCP实现聊天/TcpClientDemo01.java
@@ -0,0 +1,43 @@
+package com.tlyon.TCP实现聊天;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TcpClientDemo01 {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        Socket socket = null;
+        OutputStream os = null;
+//        1.要知道服务器的地址
+        try {
+            InetAddress serverIP = InetAddress.getByName("127.0.0.1");
+            int port = 9999;
+//           2. 创建一个socket连接
+            socket = new Socket(serverIP, port);
+//             3.发送消息 IO流
+            os = socket.getOutputStream();
+            os.write("你好，欢迎学习狂神说java".getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ 网络编程/src/com/tlyon/TCP实现聊天/TcpServerDemo01.java
@@ -0,0 +1,75 @@
+package com.tlyon.TCP实现聊天;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServerDemo01 {
+
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        ByteArrayOutputStream baos = null;
+        InputStream is = null;
+        Socket socket = null;
+        ServerSocket serverSocket = null;
+        try {
+//            1.我得有一个地址
+            serverSocket = new ServerSocket(9999);
+//            2.等待客户端连接过来
+            socket = serverSocket.accept();
+//            3.读取客户端的消息
+            is = socket.getInputStream();
+            /*
+                有缺陷，如果出现中文就会乱码。因为每次读到msg里面都是1024个字符。会中断字符。造成乱码。
+                byte[] buffer = new byte[1024];
+                            int len;
+                            while ((len = is.read(buffer)) != -1) {
+                                String msg = new String(buffer, 0, len);
+                                System.out.println(msg);
+                            }
+             */
+            baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            System.out.println(baos.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
