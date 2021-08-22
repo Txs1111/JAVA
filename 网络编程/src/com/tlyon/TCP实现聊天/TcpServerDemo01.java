9384ff13f8a2e8e9910703d5ecb418fa9e8a0e3c
L_yon <1831069264@qq.com>
2021-08-22 15:34:14 +0800

    update

--- 网络编程/src/com/tlyon/TCP实现聊天/TcpServerDemo01.java
+++ 网络编程/src/com/tlyon/TCP实现聊天/TcpServerDemo01.java
@@ -18,10 +18,12 @@ public class TcpServerDemo01 {
         try {
 //            1.我得有一个地址
             serverSocket = new ServerSocket(9999);
+//            服务器循环接受信息
+            while (true) {
 //            2.等待客户端连接过来
-            socket = serverSocket.accept();
+                socket = serverSocket.accept();
 //            3.读取客户端的消息
-            is = socket.getInputStream();
+                is = socket.getInputStream();
             /*
                 有缺陷，如果出现中文就会乱码。因为每次读到msg里面都是1024个字符。会中断字符。造成乱码。
                 byte[] buffer = new byte[1024];
@@ -31,14 +33,14 @@ public class TcpServerDemo01 {
                                 System.out.println(msg);
                             }
              */
-            baos = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = is.read(buffer)) != -1) {
-                baos.write(buffer, 0, len);
+                baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = is.read(buffer)) != -1) {
+                    baos.write(buffer, 0, len);
+                }
+                System.out.println(baos.toString());
             }
-            System.out.println(baos.toString());
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
