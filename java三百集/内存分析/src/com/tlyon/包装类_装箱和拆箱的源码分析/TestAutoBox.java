d6c3872e52f99bcf89e3b853b802f3b9fb00560f
L_yon <1831069264@qq.com>
2021-09-07 20:54:45 +0800

    update

--- java三百集/内存分析/src/com/tlyon/包装类_装箱和拆箱的源码分析/TestAutoBox.java
+++ java三百集/内存分析/src/com/tlyon/包装类_装箱和拆箱的源码分析/TestAutoBox.java
@@ -32,18 +32,8 @@ public class TestAutoBox {
 
 
 
-        System.out.println("1111111111111111111111111111111111111111111111111");
 
 
-        String s0 = "kvill";
-        String s1 = new String("kvill");
-        String s2 = new String("kvill");
-        System.out.println(s0 == s1);
-        System.out.println("**********");
-        s1.intern();
-        s2 = s2.intern(); //把常量池中"kvill"的引用赋给s2
-        System.out.println(s0 == s1);
-        System.out.println(s0 == s1.intern());
-        System.out.println(s0 == s2);
+
     }
 }
