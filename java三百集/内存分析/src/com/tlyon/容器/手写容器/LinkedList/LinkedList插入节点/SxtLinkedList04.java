7a54637c47b0b9032044fa8c6d647cb0a59747e5
L_yon <1831069264@qq.com>
2021-09-08 17:00:30 +0800

    update

--- java三百集/内存分析/src/com/tlyon/容器/手写容器/LinkedList/LinkedList插入节点/SxtLinkedList04.java
+++ java三百集/内存分析/src/com/tlyon/容器/手写容器/LinkedList/LinkedList插入节点/SxtLinkedList04.java
@@ -1,67 +1,67 @@
-package com.tlyon.容器.手写容器.LinkedList;
+package com.tlyon.容器.手写容器.LinkedList.LinkedList插入节点;
 
+
+import com.tlyon.容器.手写容器.LinkedList.Node;
 
 /**
  * 自定义一个链表
- * 增加remove
+ * 插入节点
  *
  * @author Administrator
  */
-public class SxtLinkedList03 {
-
+public class SxtLinkedList04 {
     private Node first;
     private Node last;
-
     private int size;
+
+    public void add(int index, Object obj) {
+        Node newNode = new Node(obj);
+        Node temp = getNode(index);
+        if (temp != null) {
+            Node up = temp.previous;
+            up.next = newNode;
+            newNode.previous = up;
+            newNode.next = temp;
+            temp.previous = newNode;
+        }
+    }
 
 
     public void remove(int index) {
         Node temp = getNode(index);
-
         if (temp != null) {
             Node up = temp.previous;
             Node down = temp.next;
-
             if (up != null) {
                 up.next = down;
             }
-
             if (down != null) {
                 down.previous = up;
             }
-
             //被删除的元素是第一个元素时
             if (index == 0) {
                 first = down;
             }
-
             //被删除的元素是最后一个元素时
             if (index == size - 1) {
                 last = up;
             }
-
             size--;
         }
-
     }
 
     //[]
     //["a","b","c","d","e","f"]           2
     public Object get(int index) {
-
         if (index < 0 || index > size - 1) {
             throw new RuntimeException("索引数字不合法:" + index);
         }
-
         Node temp = getNode(index);
-
         return temp != null ? temp.element : null;
     }
 
-
     public Node getNode(int index) {
         Node temp = null;
-
         if (index <= (size >> 1)) {   //size>>1相当于除以2
             temp = first;
             for (int i = 0; i < index; i++) {
@@ -73,32 +73,24 @@ public class SxtLinkedList03 {
                 temp = temp.previous;
             }
         }
-
         return temp;
     }
 
-
     public void add(Object obj) {
         Node node = new Node(obj);
-
         if (first == null) {
 //			node.previous = null;
 //			node.next = null;
             first = node;
             last = node;
         } else {
-
             node.previous = last;
             node.next = null;
 
             last.next = node;
             last = node;
-
         }
-
         size++;
-
-
     }
 
     @Override
@@ -112,12 +104,11 @@ public class SxtLinkedList03 {
             temp = temp.next;
         }
         sb.setCharAt(sb.length() - 1, ']');
-
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        SxtLinkedList03 list = new SxtLinkedList03();
+        SxtLinkedList04 list = new SxtLinkedList04();
 
         list.add("a");
         list.add("b");
@@ -127,13 +118,8 @@ public class SxtLinkedList03 {
         list.add("f");
 
         System.out.println(list);
-        list.remove(3);
+        list.add(3, "老高");
         System.out.println(list);
-        list.remove(0);
-        System.out.println(list);
-        list.remove(3);
-        System.out.println(list);
+
     }
-
-
 }
--- java三百集/内存分析/src/com/tlyon/容器/手写容器/LinkedList/LinkedList移除元素/SxtLinkedList03.java
+++ java三百集/内存分析/src/com/tlyon/容器/手写容器/LinkedList/LinkedList移除元素/SxtLinkedList03.java
@@ -1,87 +1,54 @@
-package com.tlyon.容器.手写容器.LinkedList;
+package com.tlyon.容器.手写容器.LinkedList.LinkedList移除元素;
 
+
+import com.tlyon.容器.手写容器.LinkedList.Node;
 
 /**
  * 自定义一个链表
- * 插入节点
+ * 增加remove
  *
  * @author Administrator
  */
-public class SxtLinkedList04 {
-
+public class SxtLinkedList03 {
     private Node first;
     private Node last;
-
     private int size;
-
-
-    public void add(int index, Object obj) {
-
-        Node newNode = new Node(obj);
-        Node temp = getNode(index);
-
-        if (temp != null) {
-            Node up = temp.previous;
-
-            up.next = newNode;
-            newNode.previous = up;
-
-            newNode.next = temp;
-            temp.previous = newNode;
-
-        }
-
-
-    }
-
 
     public void remove(int index) {
         Node temp = getNode(index);
-
         if (temp != null) {
             Node up = temp.previous;
             Node down = temp.next;
-
             if (up != null) {
                 up.next = down;
             }
-
             if (down != null) {
                 down.previous = up;
             }
-
             //被删除的元素是第一个元素时
             if (index == 0) {
                 first = down;
             }
-
             //被删除的元素是最后一个元素时
             if (index == size - 1) {
                 last = up;
             }
-
             size--;
         }
-
     }
 
     //[]
     //["a","b","c","d","e","f"]           2
     public Object get(int index) {
-
         if (index < 0 || index > size - 1) {
             throw new RuntimeException("索引数字不合法:" + index);
         }
-
         Node temp = getNode(index);
-
         return temp != null ? temp.element : null;
     }
 
-
     public Node getNode(int index) {
         Node temp = null;
-
         if (index <= (size >> 1)) {   //size>>1相当于除以2
             temp = first;
             for (int i = 0; i < index; i++) {
@@ -93,32 +60,25 @@ public class SxtLinkedList04 {
                 temp = temp.previous;
             }
         }
-
         return temp;
     }
 
 
     public void add(Object obj) {
         Node node = new Node(obj);
-
         if (first == null) {
 //			node.previous = null;
 //			node.next = null;
             first = node;
             last = node;
         } else {
-
             node.previous = last;
             node.next = null;
 
             last.next = node;
             last = node;
-
         }
-
         size++;
-
-
     }
 
     @Override
@@ -132,12 +92,11 @@ public class SxtLinkedList04 {
             temp = temp.next;
         }
         sb.setCharAt(sb.length() - 1, ']');
-
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        SxtLinkedList04 list = new SxtLinkedList04();
+        SxtLinkedList03 list = new SxtLinkedList03();
 
         list.add("a");
         list.add("b");
@@ -147,10 +106,11 @@ public class SxtLinkedList04 {
         list.add("f");
 
         System.out.println(list);
-        list.add(3, "老高");
+        list.remove(3);
         System.out.println(list);
-
+        list.remove(0);
+        System.out.println(list);
+        list.remove(3);
+        System.out.println(list);
     }
-
-
 }
